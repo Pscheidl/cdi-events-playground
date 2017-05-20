4db0b3ca1f35aad969dbbd19fc939458b09cca72
Pavel Pscheidl <pavel.junior@pscheidl.cz>
2017-05-20 20:06:20 +0200

    Removed license headers.

--- src/main/java/cool/javaee/cdi/events/listeners/ImportantEventListener.java
+++ src/main/java/cool/javaee/cdi/events/listeners/ImportantEventListener.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package cool.javaee.cdi.events.listeners;
 
 import cool.javaee.cdi.events.listeners.qualifiers.Important;
